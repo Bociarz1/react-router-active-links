9fb9e9feadd6ce6a4e008ade588acc9d54c5d61f
Bociarz1 <mikrofoncrk@gmail.com>
2022-07-08 21:27:53 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import Browser Router in index.js
+// import LINK, ROUTE and ROUTES 
+import React from "react";
+import { NavLink, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+
+type navLinkStylesType = {
+  isActive: boolean
+}
 
 function App() {
+  const navLinkStyles = ({isActive}:navLinkStylesType) => {
+    return {
+    color: isActive ? 'red': 'green',
+    fontSize: isActive ? '30px': '15px'
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* NavLink contains ACTIVE class which is a boolean flag set to true when has been clicked */}
+
+    {/* you can style Active Class in 2 ways:
+    1. by adding css styles */}
+     <NavLink to='/home'>Go to HOME by NavLink</NavLink><br/>
+
+    {/* 2. by creating function({isActive}) */}
+    <NavLink style={navLinkStyles} to='/about'>Go to ABOUT by NavLink</NavLink><br/>
+      <Routes>
+        <Route
+          path='/home'
+          element={<Home/>}   
+        />
+         <Route
+          path='/about'
+          element={<About/>}   
+        />
+      </Routes>
+    </>
   );
 }
 
